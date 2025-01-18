bde312d572496b32fd60b80657ba9796d035444b
dev.goraebap <dev.goraebap@gmail.com>
2025-01-18 12:23:57 +0900

    Feat: update drizzle service

--- src/shared/database/drizzle.service.ts
+++ src/shared/database/drizzle.service.ts
@@ -1,46 +1,45 @@
-import {Injectable, Logger} from "@nestjs/common";
-import {drizzle, LibSQLDatabase} from "drizzle-orm/libsql";
-import {createClient} from "@libsql/client";
-import {SQLiteTransaction} from "drizzle-orm/sqlite-core";
-import {AsyncLocalStorage} from "async_hooks";
+import { Injectable, Logger } from '@nestjs/common';
+import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+import { SQLiteTransaction } from 'drizzle-orm/sqlite-core';
+import { AsyncLocalStorage } from 'async_hooks';
 
-import {EnvService} from "../env";
+import { EnvService } from '../env';
 
 interface TransactionStore {
-    transaction?: SQLiteTransaction<any, any, any, any>;
+  transaction?: SQLiteTransaction<any, any, any, any>;
 }
 
 @Injectable()
 export class DrizzleService {
 
-    private readonly storage: AsyncLocalStorage<TransactionStore> = new AsyncLocalStorage();
+  private readonly storage: AsyncLocalStorage<TransactionStore> = new AsyncLocalStorage();
 
-    private _db: LibSQLDatabase;
+  constructor(
+    private readonly envService: EnvService,
+  ) {
+    Logger.debug('Drizzle DB Init');
+    const { url, authToken } = this.envService.getDBEnv();
+    const client = createClient({ url, authToken });
+    this._db = drizzle(client);
+  }
 
-    get db() {
-        if (!this._db) {
-            throw new Error('not connect to DB.');
-        }
+  private _db: LibSQLDatabase;
 
-        /**
-         * 트랜잭션이 존재하는 상태이면 트랜잭션을 사용하도록 합니다.
-         */
-        return this.storage.getStore()?.transaction ?? this._db;
+  get db() {
+    if (!this._db) {
+      throw new Error('not connect to DB.');
     }
 
-    constructor(
-        private readonly envService: EnvService
-    ) {
-        Logger.debug("Drizzle DB Init");
-        const {url, authToken} = this.envService.getDBEnv();
-        console.log(url, authToken);
-        const client = createClient({url, authToken});
-        this._db = drizzle(client);
-    }
+    /**
+     * 트랜잭션이 존재하는 상태이면 트랜잭션을 사용하도록 합니다.
+     */
+    return this.storage.getStore()?.transaction ?? this._db;
+  }
 
-    async runInTx<T>(callback: () => Promise<T>): Promise<T> {
-        return await this._db.transaction(async t => {
-            return this.storage.run({transaction: t}, callback);
-        });
-    }
+  async runInTx<T>(callback: () => Promise<T>): Promise<T> {
+    return await this._db.transaction(async t => {
+      return this.storage.run({ transaction: t }, callback);
+    });
+  }
 }
