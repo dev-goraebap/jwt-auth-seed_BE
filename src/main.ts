0a142efdd2b2397376d58a6a6f759747a6dfc20c
dev.goraebap <dev.goraebap@gmail.com>
2025-01-18 20:25:35 +0900

    Style: update tab size

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,40 @@
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 
-import {MainModule} from "./main.module";
-import {EnvService} from "./shared/env";
+import { MainModule } from './main.module';
+import { EnvService } from './shared/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainModule);
+    const app = await NestFactory.create(MainModule);
 
-  app.setGlobalPrefix('api');
-  app.enableVersioning();
+    app.enableCors({
+        origin: [
+            'https://jwt-auth-seed-926d8.web.app', // 웹 배포 환경
+            'http://localhost:4200',  // 웹 개발 환경
+            'http://localhost:56979',  // 웹 개발 환경
+            'capacitor://localhost',  // Capacitor
+            'ionic://localhost',      // Ionic
+            'file://*',               // 안드로이드 WebView
+        ],
+    });
 
-  const envService = app.get(EnvService);
+    app.setGlobalPrefix('api');
+    app.enableVersioning();
 
-  const config = new DocumentBuilder()
-      .setTitle('JWT AUTH SEED API')
-      .setDescription('JWT Authentication Seed API')
-      .addBearerAuth()
-      .setVersion('1.0')
-      .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+    const envService = app.get(EnvService);
 
-  const { name, port } = envService.getAppEnv();
-  await app.listen(port ?? 8000, () => Logger.log(`[${name}] Server started on port ${port}`));
+    const config = new DocumentBuilder()
+        .setTitle('JWT AUTH SEED API')
+        .setDescription('JWT Authentication Seed API')
+        .addBearerAuth()
+        .setVersion('1.0')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+
+    const { name, port } = envService.getAppEnv();
+    await app.listen(port ?? 8000, () => Logger.log(`[${name}] Server started on port ${port}`));
 }
+
 bootstrap();
