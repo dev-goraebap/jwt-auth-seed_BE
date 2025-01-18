27118a1cbc2be954fd51e110827fc7babd1feb88
dev.goraebap <dev.goraebap@gmail.com>
2025-01-18 20:27:14 +0900

    Feat: add nickname update method

--- src/app/user/user.facade.ts
+++ src/app/user/user.facade.ts
@@ -4,7 +4,13 @@ import {UserModel, UserService} from "src/features/user";
 import {UserTokenService} from "src/features/user-token";
 import {DrizzleService} from "src/shared/database";
 
-import {IsEmailTakenResultDTO, UpdatePasswordToOtpDTO, WithdrawDTO} from "./user.dto";
+import {
+    IsEmailTakenResultDTO,
+    ProfileResultDTO,
+    UpdateNicknameDTO,
+    UpdatePasswordToOtpDTO,
+    WithdrawDTO,
+} from './user.dto';
 
 @Injectable()
 export class UserFacade {
@@ -35,5 +41,10 @@ export class UserFacade {
             await this.userTokenService.removeByUserId(user.id);
             await this.userService.remove(user);
         });
+    }
+
+    async updateNickname(user: UserModel, dto: UpdateNicknameDTO) {
+        user = await this.userService.updateNickname(user, dto.nickname);
+        return ProfileResultDTO.from(user);
     }
 }
